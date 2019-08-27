794afb8bae2ea8489b2be10e6b5c195faf7cb564
Winfesk <winfesk@gmail.com>
2019-08-27 16:59:45 +0300

    updated for users

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello, Udacity!')
+        self.response.write('Plese wait for a while...we are just goating this site')
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
