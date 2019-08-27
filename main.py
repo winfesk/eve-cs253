0e1d262de940b8e91e256632fed4ff9869d991dc
Winfesk <winfesk@gmail.com>
2019-08-27 17:04:58 +0300

    update update update

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import webapp2
 
+form="""
+    <form action="http://www.google.com/search">
+        <input name="q" /> <input type="submit" />
+    </form>
+"""
+
+
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Plese wait for a while...we are just goating this site')
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
