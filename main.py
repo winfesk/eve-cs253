940c4115dc6d652fb9ce07729cee265df1603815
Winfesk <winfesk@gmail.com>
2019-08-27 18:56:26 +0300

    update a bit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import webapp2
 
 form="""
-    <form action="http://www.google.com/search">
+    <form action="/testform">
         <input name="q" /> <input type="submit" />
     </form>
 """
@@ -10,6 +10,11 @@ form="""
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.write(q)
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
