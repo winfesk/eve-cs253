b3fc980e212cac8376334df0963e620b5a0cc7dc
Winfesk <winfesk@gmail.com>
2019-09-08 22:58:07 +0300

    updated for udacity

--- main.py
+++ main.py
@@ -18,37 +18,37 @@ form="""
     </form>
 """
 
+def valid_month(month):
+    if len(month) < 1:
+        return None
+    month = lowerCases(month)
+    if month in months:
+        return month
+    return None
+
+def lowerCases(month):
+    result = month[0].upper()
+    i = 0
+    for letter in month:
+        if i != 0:
+            result = result + letter.lower()
+        i = 1
+    return result
+
+def valid_day(day):
+    if day.isdigit():
+        if int(day) >= 1 and int(day) <= 31:
+            return int(day)
+    return None
+
+def valid_year(year):
+    if year.isdigit():
+        if int(year) >= 1900 and int(year) <= 2020:
+            return int(year)
+    return None
+
 
 class HelloWebapp2(webapp2.RequestHandler):
-    def valid_month(month):
-        if len(month) < 1:
-            return None
-        month = lowerCases(month)
-        if month in months:
-            return month
-        return None
-
-    def lowerCases(month):
-        result = month[0].upper()
-        i = 0
-        for letter in month:
-            if i != 0:
-                result = result + letter.lower()
-            i = 1
-        return result
-
-    def valid_day(day):
-        if day.isdigit():
-            if int(day) >= 1 and int(day) <= 31:
-                return int(day)
-        return None
-
-    def valid_year(year):
-        if year.isdigit():
-            if int(year) >= 1900 and int(year) <= 2020:
-                return int(year)
-        return None
-
     def get(self):
         self.response.write(form)
 
