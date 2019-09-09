40de350f594a5ef7821e548f191511f9f979889c
Winfesk <winfesk@gmail.com>
2019-09-09 03:30:41 +0300

    updated for udacity

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ form="""
          What is your birthday?
          <br>
          <label> Month
-            <input type="text" placeholder="december" name="month">
+            <input type="text" placeholder="december" name="month" value="%(month)s">
          </label>
          <label> Day
-            <input type="text" placeholder="19" name="day">
+            <input type="text" placeholder="19" name="day" value="%(day)s">
          </label>
          <label> Year
-            <input type="text" placeholder="1999" name="year">
+            <input type="text" placeholder="1999" name="year" value="%(year)s">
          </label>
          <div style="color: red">%(error)s</div>
          <br>
@@ -63,19 +63,27 @@ def valid_year(year):
 
 
 class HelloWebapp2(webapp2.RequestHandler):
-    def write_form(self, error=""):
-        self.response.write(form % {"error": error})
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.write(form % {"error": error,
+                                    "month": month,
+                                    "day": day,
+                                    "year": year})
 
     def get(self):
         self.write_form()
 
     def post(self):
-        user_month = valid_month(self.request.get('month'))
-        user_day = valid_day(self.request.get('day'))
-        user_year = valid_year(self.request.get('year'))
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
+
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
 
         if not (user_month and user_day and user_year):
-            self.write_form("That doesn't look valid to me, friend.")
+            self.write_form("That doesn't look valid to me, friend.",
+                            user_month, user_day, user_year)
         else:
             self.response.write("Thanks! That's a totally valid day!")
 
